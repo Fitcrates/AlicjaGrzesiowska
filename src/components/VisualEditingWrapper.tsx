e65d45b0f7b5445b76db39d130b6e7f63476c134
Fitcrates <aeonofshreds@gmail.com>
2026-06-06 15:51:04 +0200

    awdwad

--- src/components/VisualEditingWrapper.tsx
+++ src/components/VisualEditingWrapper.tsx
@@ -2,13 +2,23 @@
 
 import { VisualEditing } from 'next-sanity/visual-editing'
 import { useIsPresentationTool } from 'next-sanity/hooks'
+import { useEffect } from 'react'
 
 export default function VisualEditingWrapper() {
   const isPresentationTool = useIsPresentationTool()
 
-  if (!isPresentationTool) {
-    return null
-  }
+  useEffect(() => {
+    if (!isPresentationTool) {
+      const style = document.createElement('style')
+      style.innerHTML = `
+        sanity-visual-editing { display: none !important; }
+      `
+      document.head.appendChild(style)
+      return () => {
+        document.head.removeChild(style)
+      }
+    }
+  }, [isPresentationTool])
 
   return <VisualEditing />
 }
